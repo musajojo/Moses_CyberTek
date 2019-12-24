fd5adb21d16a56a78e115fc8ca436d4ec1046754
Moses Dee <58441332+musajojo@users.noreply.github.com>
2019-12-23 20:31:19 -0500

    CyberTek Code

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -1,12 +1,8 @@
-package day01;
+package Day01;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        //sout is short cut for below
-
-        System.out.println("Hello Batch 15!!!");
     }
 }
