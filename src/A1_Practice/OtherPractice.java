fda1ab81a689463d63db6901592d3387fe36d2e4
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-01-01 00:17:32 -0500

    Cybertek Code

---
+++ src/A1_Practice/OtherPractice.java
@@ -0,0 +1,22 @@
+package A1_Practice;
+import java.util.*;
+public class OtherPractice {
+    public static void main(String[] args) {
+
+        System.out.println("Control Structures");
+        System.out.println();
+
+        System.out.println("If Statement");
+        int age = 20;
+        if (age<20 ){
+            System.out.println("You are less than 20");
+        } else{
+            System.out.println("Ok Bye");
+        }
+
+        System.out.println();
+        System.out.println("Loops");
+
+    }
+
+}
--- src/A1_Practice/ReplWork.java
+++ src/A1_Practice/ReplWork.java
@@ -2,10 +2,15 @@ package A1_Practice;
 import java.util.*;
 public class ReplWork {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
 
     }
 }
+
+/*
+
+ */
 /*
 
  */
