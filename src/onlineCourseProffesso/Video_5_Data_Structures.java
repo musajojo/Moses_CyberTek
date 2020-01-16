e600b2ba2935158650b09ad925105e537c0b7f42
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-01-16 11:34:43 -0500

    Cybertek Code

---
+++ src/onlineCourseProffesso/Video_5_Data_Structures.java
@@ -0,0 +1,82 @@
+package onlineCourseProffesso;
+
+public class Video_5_Data_Structures {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+/*
+package net.javavideotutorials.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataStructures
+{
+	public static void main(String[] args)
+	{
+		Map<String, List<String>> makeModel = new HashMap<String, List<String>>();
+
+		List<String> hondaList = new ArrayList<String>();
+		hondaList.add("Civic");
+		hondaList.add("Prelude");
+
+		List<String> toyotaList = new ArrayList<String>();
+		toyotaList.add("Corolla");
+		toyotaList.add("Rav 4");
+
+    makeModel.put("Honda", hondaList);
+    makeModel.put("Toyota", toyotaList);
+
+    System.out.println(makeModel.get("Honda"));
+    System.out.println(makeModel.get("Toyota"));
+    System.out.println(makeModel.get("Ford"));
+    System.out.println(makeModel.get("Audi"));
+
+    // Uncomment next line if you want to execute the arrayList code
+    //arrayListMethod();
+
+    // Uncomment next line if you want to execute the arrayList code
+    //arrayMethod();
+	}
+
+	public static void arrayListMethod ()
+	{
+	  List<Character> arrayList = new ArrayList<Character>();
+
+    arrayList.add('H');
+    arrayList.add('E');
+    arrayList.add('L');
+    arrayList.add('L');
+    arrayList.add('O');
+    arrayList.add('!');
+
+    arrayList.remove(5);
+
+    for (Character s : arrayList)
+    {
+      System.out.print(s);
+    }
+	}
+
+	public static void arrayMethod ()
+	{
+    char[] charArray = new char[5];
+
+    charArray[0] = 'H';
+    charArray[1] = 'E';
+    charArray[2] = 'L';
+    charArray[3] = 'L';
+    charArray[4] = 'O';
+
+    for (char c : charArray)
+    {
+      System.out.print(c);
+    }
+	}
+}
+ */
