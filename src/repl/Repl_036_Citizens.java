b108dc12102dca8b9663a3fc70da32f4c538e8a0
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-01-15 20:47:32 -0500

    Cybertek Code

---
+++ src/repl/Repl_036_Citizens.java
@@ -0,0 +1,51 @@
+package repl;
+import java.util.*;
+public class Repl_036_Citizens {
+    public static void main(String[] args) {
+
+
+    }
+}
+/*
+036. Citizens
+Instructions from your teacher:
+
+-Declare int variables: seniorCitizens, nonSeniorCitizens, age
+-Create a Scanner object
+- Ask user to enter count for seniorCitizens
+    and nonSeniorCitizens using message:
+"Enter current count for seniorCitizens and nonSeniorCitizens:"
+
+-Ask user to enter age:
+"What is new citizen's age?"
+
+-if citizen's age is more than or equals to 65 then print "Senior Citizen"
+    and increment seniorCitizens variable by 1.
+
+-if citizen's age is less than 65 then print "Non-Senior Citizen"
+    and increment nonSeniorCitizens variable by 1.
+
+-At last, print another message:
+"New seniorCitizens count ValueOfVariable"
+"New nonSeniorCitizens count ValueOfVariable"
+
+Example Run:
+
+Enter current count for seniorCitizens and nonSeniorCitizens:
+5
+4
+What is new citizen's age?
+66
+Senior Citizen
+New seniorCitizens count 6
+New nonSeniorCitizens count 4
+ */
+/*
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //WRITE YOUR CODE HERE
+  }
+}
+ */
