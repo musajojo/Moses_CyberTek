f1e0da5f6a0d80e2421814d774661ac4483db460
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-01-21 22:13:35 -0500

    Cybertek Code

--- src/repl/Repl_036_Citizens.java
+++ src/repl/Repl_036_Citizens.java
@@ -1,9 +1,27 @@
 package repl;
+import javax.tools.SimpleJavaFileObject;
 import java.util.*;
+
 public class Repl_036_Citizens {
     public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int seniorCitizens, nonSeniorCitizens, age;
 
+        System.out.println("Enter current count for seniorCitizens and nonSeniorCitizens:");
+        seniorCitizens = scan.nextInt();
+        nonSeniorCitizens = scan.nextInt();
+        System.out.println("What is new citizen's age?");
+        age = scan.nextInt();
+        if (age >= 65){
+            System.out.println("Senior Citizen");
+            seniorCitizens = seniorCitizens + 1;
+        }else {
+            System.out.println("Non-Senior Citizen");
+            nonSeniorCitizens = nonSeniorCitizens + 1;
+        }
 
+        System.out.println("New seniorCitizens count " + seniorCitizens);
+        System.out.println("New nonSeniorCitizens count " + nonSeniorCitizens);
     }
 }
 /*
