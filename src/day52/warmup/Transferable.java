16cd41d37a0644a0d69b67f4958cc3f4af0b5ab9
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-02-03 12:03:08 -0500

    Cybertek Code

---
+++ src/day52/warmup/Transferable.java
@@ -0,0 +1,9 @@
+package day52.warmup;
+
+public interface Transferable {
+
+    public abstract void transferAll(Account otherAccount);
+
+    // public abstract void transferAmount(Account otherAccount , int amountToTransfer);
+
+}
