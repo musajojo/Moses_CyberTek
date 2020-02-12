bc4356b9f346832a6215c563530a4c8d35fc9129
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-02-11 21:45:20 -0500

    Cybertek Code

--- src/repl/repls_51_to_100/Repl_066_Merge_them.java
+++ src/repl/repls_51_to_100/Repl_066_Merge_them.java
@@ -9,6 +9,26 @@ public class Repl_066_Merge_them {
         String word1 = scan.next();
         String word2 = scan.next();
 
+        String s = "";
+        int i = 0;
+
+        if (word1.length() != 3 || word2.length() != 3){
+            System.out.println("cannot merge");
+        }else{
+        while (i < word1.length() && i < word2.length()){
+            s += word1.charAt(i) +""+ word2.charAt(i);
+            i++;
+        }
+        while (i < word1.length() ){
+            s += word1.charAt(i);
+            i++;
+        }
+        while (i < word2.length()){
+            s += word2.charAt(i);
+            i++;
+        }
+        System.out.println(s);
+        }
     }
 }
 /*
